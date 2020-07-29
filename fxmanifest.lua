1dfe4f5dff2331063f12ea6dd9e7f88cd465ae71
Kevin0M16 <kevin@nmscoordinates.com>
2020-07-28 20:49:26 -0500

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Kevin_M16'
+description 'Water Control'
+version '1.0.0'
+
+replace_level_meta 'gta5'
+files {
+    'gta5.meta',
+    'water.xml'
+}
